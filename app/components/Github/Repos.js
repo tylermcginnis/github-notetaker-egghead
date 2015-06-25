87902ac419df394cb84a3e2fd2f4e10aebf702d7
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-06-25 01:00:16 -0600

    Add UserProfile and Repos cleanup

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -6,10 +6,21 @@ var Repos = React.createClass({
     repos: React.PropTypes.array.isRequired
   },
   render: function(){
+    var repos = this.props.repos.map(function(repo, index){
+      return (
+        <li className="list-group-item" key={index}>
+          {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+          {repo.description && <p> {repo.description}</p>}
+        </li>
+      );
+    });
     return (
-      <div> REPOS<br />
-        Username: {this.props.username} <br />
-        REPOS: {this.props.repos} </div>
+      <div>
+        <h3> User Repos </h3>
+        <ul className="list-group">
+          {repos}
+        </ul>
+      </div>
     )
   }
 });
