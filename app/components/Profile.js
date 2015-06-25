42987a5f18732ca05c5f4145f743e454347c51b4
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-06-25 01:18:59 -0600

    Add final file

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -16,8 +16,7 @@ var Profile = React.createClass({
       repos: []
     }
   },
-  componentDidMount: function(){
-    this.ref = new Firebase('https://github-note-taker.firebaseio.com');
+  init: function(){
     var childRef = this.ref.child(this.getParams().username);
     this.bindAsArray(childRef, 'notes');
 
@@ -29,8 +28,16 @@ var Profile = React.createClass({
         });
       }.bind(this));
   },
+  componentDidMount: function(){
+    this.ref = new Firebase('https://github-note-taker.firebaseio.com');
+    this.init();
+  },
   componentWillUnmount: function(){
     this.unbind('notes');
+  },
+  componentWillReceiveProps: function(){
+    this.unbind('notes');
+    this.init();
   },
   handleAddNote: function(newNote){
     this.ref.child(this.getParams().username).set(this.state.notes.concat([newNote]));
