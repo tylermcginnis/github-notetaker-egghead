6509d369aef56e6aa109dc820229c4c3691cfc80
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-06-24 23:55:14 -0600

    Add menu transition

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var RouteHandler = require('react-router').RouteHandler;
+var SearchGithub = require('./SearchGithub');
 
 var Main = React.createClass({
   render: function(){
@@ -7,7 +8,7 @@ var Main = React.createClass({
       <div className="main-container">
         <nav className="navbar navbar-default" role="navigation">
           <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-            MENU
+            <SearchGithub />
           </div>
         </nav>
         <div className="container">
