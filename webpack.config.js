7c0368373c0e05d6e7a6f40eb98ee66b38b846e0
ijones16 <jones58ian@gmail.com>
2015-11-08 15:53:00 -0800

    added readme, new webpack config with babel 6

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
+var buildPath = require('path').join(__dirname, 'public');
 module.exports = {
   entry: "./app/App.js",
   output: {
-    filename: "public/bundle.js"
+    path: buildPath,
+    filename: "bundle.js",
+    publicPath: '/assets/'
+  },
+  devServer: {
+    inline: true,
+    progress: true,
+
+    // parse host and port from env so this is easy
+    // to customize
+    host: process.env.HOST,
+    port: process.env.PORT
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel'
+        loader: 'babel',
+        query: {
+          // https://github.com/babel/babel-loader#options
+          cacheDirectory: true,
+          presets: ['react']
+        }
       }
     ]
   }
