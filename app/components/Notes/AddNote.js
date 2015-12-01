5591beb77d5a377ce404396bf177804ed9a2d798
ijones16 <jones58ian@gmail.com>
2015-12-01 10:56:45 -0800

    updated react and react-router

--- app/components/Notes/AddNote.js
+++ app/components/Notes/AddNote.js
@@ -6,20 +6,20 @@ var AddNote = React.createClass({
     addNote: React.PropTypes.func.isRequired
   },
   handleSubmit: function(){
-    var newNote = this.refs.note.getDOMNode().value;
-    this.refs.note.getDOMNode().value = '';
+    var newNote = this.refs.note.value;
+    this.refs.note.value = '';
     this.props.addNote(newNote);
   },
   render: function(){
     return (
-      <div className="input-group">
-        <input type="text" className="form-control" ref="note" placeholder="Add New Note" />
+        <div className="input-group">
+          <input type="text" className="form-control" ref="note" placeholder="Add New Note" />
         <span className="input-group-btn">
           <button className="btn btn-default" type="button" onClick={this.handleSubmit}> Submit </button>
         </span>
-      </div>
+        </div>
     )
   }
 });
 
-module.exports = AddNote;
+module.exports = AddNote;;
