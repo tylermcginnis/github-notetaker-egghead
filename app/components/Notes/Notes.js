045a3ac858615dfc56f197c8f8a08a90dd0d6c22
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-12-13 11:40:57 -0700

    Updates for video #5

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,12 +1,12 @@
 var React = require('react');
+var NotesList = require('./NotesList');
 
 var Notes = React.createClass({
   render: function(){
     return (
       <div>
-        <p>NOTES </p>
-        <p> {this.props.notes}</p>
-
+        <h3> Notes for {this.props.username} </h3>
+        <NotesList notes={this.props.notes} />
       </div>
     )
   }
--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,10 +1,13 @@
 var React = require('react');
 var Router = require('react-router');
-var Repos = require('./Github/Repos')
-var UserProfile = require('./Github/UserProfile')
-var Notes = require('./Notes/Notes')
+var Repos = require('./Github/Repos');
+var UserProfile = require('./Github/UserProfile');
+var Notes = require('./Notes/Notes');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 
 var Profile = React.createClass({
+  mixins: [ReactFireMixin],
   getInitialState: function(){
     return {
       notes: [1,2,3],
@@ -14,18 +17,25 @@ var Profile = React.createClass({
       repos: ['a', 'b', 'c']
     }
   },
+  componentDidMount: function(){
+    this.ref = new Firebase('https://github-note-taker.firebaseio.com/');
+    var childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnmount: function(){
+    this.unbind('notes');
+  },
   render: function(){
-    console.log(this.props)
     return (
       <div className="row">
         <div className="col-md-4">
           <UserProfile username={this.props.params.username} bio={this.state.bio} />
         </div>
         <div className="col-md-4">
-          <Repos repos={this.state.repos}/>
+          <Repos username={this.props.params.username} repos={this.state.repos}/>
         </div>
         <div className="col-md-4">
-          <Notes notes={this.state.notes} />
+          <Notes username={this.props.params.username} notes={this.state.notes} />
         </div>
       </div>
     )
