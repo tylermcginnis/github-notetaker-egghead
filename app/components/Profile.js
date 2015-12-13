6143a8c949ec1b663ef3de6dc9fab13437aef523
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-12-13 16:29:34 -0700

    Updates for Video #7

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -25,6 +25,9 @@ var Profile = React.createClass({
   componentWillUnmount: function(){
     this.unbind('notes');
   },
+  handleAddNote: function(newNote){
+    this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote)
+  },
   render: function(){
     return (
       <div className="row">
@@ -35,7 +38,10 @@ var Profile = React.createClass({
           <Repos username={this.props.params.username} repos={this.state.repos}/>
         </div>
         <div className="col-md-4">
-          <Notes username={this.props.params.username} notes={this.state.notes} />
+          <Notes
+            username={this.props.params.username}
+            notes={this.state.notes}
+            addNote={this.handleAddNote} />
         </div>
       </div>
     )
