6e987e42274175acfcf81d57e38a24cb6c494ac0
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-12-13 19:18:54 -0700

    Updates for video 10 - dynamic ui

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -6,10 +6,20 @@ var Repos = React.createClass({
     repos: React.PropTypes.array.isRequired
   },
   render: function(){
-    console.log('REPOS', this.props.repos)
+    var repos = this.props.repos.map(function(repo, index){
+      return (
+        <li className="list-group-item" key={index}>
+          {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
+          {repo.description && <p>{repo.description}</p>}
+        </li>
+      )
+    });
     return (
       <div>
-        <p> REPOS </p>
+        <h3> User Repos </h3>
+        <ul className="list-group">
+          {repos}
+        </ul>
       </div>
     )
   }
--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -6,11 +6,18 @@ var UserProfile = React.createClass({
     bio: React.PropTypes.object.isRequired
   },
   render: function(){
-    console.log('BIO', this.props.bio)
     return (
       <div>
-        <p> USER PROFILE! </p>
-        <p> Username: {this.props.username} </p>
+        {this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
+        {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
+        {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
+        {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
+        {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
+        {this.props.bio.company && <li className="list-group-item">Company: {this.props.bio.company}</li>}
+        {this.props.bio.followers && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
+        {this.props.bio.following && <li className="list-group-item">Following: {this.props.bio.following}</li>}
+        {this.props.bio.following && <li className="list-group-item">Public Repos: {this.props.bio.public_repos}</li>}
+        {this.props.bio.blog && <li className="list-group-item">Blog: <a href={this.props.bio.blog}> {this.props.bio.blog}</a></li>}
       </div>
     )
   }
