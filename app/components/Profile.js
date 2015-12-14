ffd37452ca217351a11106b11486e5f2862d348c
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-12-13 20:05:37 -0700

    Updates for 11-route-changes

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -18,19 +18,26 @@ var Profile = React.createClass({
   },
   componentDidMount: function(){
     this.ref = new Firebase('https://github-note-taker.firebaseio.com/');
-    var childRef = this.ref.child(this.props.params.username);
+    this.init(this.props.params.username)
+  },
+  componentWillReceiveProps: function(nextProps){
+    this.unbind('notes');
+    this.init(nextProps.params.username);
+  },
+  componentWillUnmount: function(){
+    this.unbind('notes');
+  },
+  init: function(username){
+    var childRef = this.ref.child(username);
     this.bindAsArray(childRef, 'notes');
 
-    helpers.getGithubInfo(this.props.params.username)
+    helpers.getGithubInfo(username)
       .then(function(data){
         this.setState({
           bio: data.bio,
           repos: data.repos
         })
       }.bind(this))
-  },
-  componentWillUnmount: function(){
-    this.unbind('notes');
   },
   handleAddNote: function(newNote){
     this.ref.child(this.props.params.username).child(this.state.notes.length).set(newNote)
--- app/components/SearchGithub.js
+++ app/components/SearchGithub.js
@@ -9,7 +9,7 @@ var SearchGithub = React.createClass({
   handleSubmit: function(){
     var username = this.usernameRef.value;
     this.usernameRef.value = '';
-    this.history.pushState(null, "profile/" + username)
+    this.history.pushState(null, "/profile/" + username)
   },
   render: function(){
     return (
