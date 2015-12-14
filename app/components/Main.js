e82e224aa67cf42dfcd9a2eb3ce49ad107c42404
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-12-13 17:57:36 -0700

    Updates for Transition branch

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var SearchGithub = require('./SearchGithub');
 
 var Main = React.createClass({
   render: function(){
@@ -6,7 +7,7 @@ var Main = React.createClass({
       <div className="main-container">
         <nav className="navbar navbar-default" role="navigation">
           <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-            MENU
+            <SearchGithub />
           </div>
         </nav>
         <div className="container">
